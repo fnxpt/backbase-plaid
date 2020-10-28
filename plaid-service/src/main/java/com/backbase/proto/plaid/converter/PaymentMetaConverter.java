db760b720f252a4349b64b111b09f8167495b9a9
sophiej <sophiej@backbase.com>
2020-10-28 16:22:08 +0100

    documentation

--- plaid-service/src/main/java/com/backbase/proto/plaid/converter/PaymentMetaConverter.java
+++ plaid-service/src/main/java/com/backbase/proto/plaid/converter/PaymentMetaConverter.java
@@ -18,7 +18,7 @@ public class PaymentMetaConverter implements AttributeConverter<PaymentMeta, Str
      * Takes a list of strings and joins then with a ',' separator so it may be stored as a column in a database.
      *
      * @param paymentMeta the list to be converted into a string
-     * @return joined string containing the contents of the list parsed in
+     * @return json string containing the contents of the list parsed in
      */
 
     @Override
@@ -27,7 +27,7 @@ public class PaymentMetaConverter implements AttributeConverter<PaymentMeta, Str
             try {
                 return objectMapper.writeValueAsString(paymentMeta);
             } catch (IOException e) {
-                log.info("context{}", e);
+                log.error("Failed to write paymentMeta: {} to database format", paymentMeta, e);
             }
         }
         return null;
@@ -36,17 +36,17 @@ public class PaymentMetaConverter implements AttributeConverter<PaymentMeta, Str
     /**
      * Takes a string separated by ',' and splits it into a list to be stored as attributes.
      *
-     * @param joined a string to be turned to a list
+     * @param json a string to be turned to a list
      * @return the list of strings to be stored as attributes
      */
 
     @Override
-    public PaymentMeta convertToEntityAttribute(String joined) {
+    public PaymentMeta convertToEntityAttribute(String json) {
         if (!StringUtils.isEmpty(StringUtils.join())) {
             try {
-                return objectMapper.readValue(joined, PaymentMeta.class);
+                return objectMapper.readValue(json, PaymentMeta.class);
             } catch (IOException e) {
-                log.info("context{}", e);
+                log.error("Failed to read Payment meta: {} to database format", json, e);
             }
 
         }
--- plaid-service/src/main/java/com/backbase/proto/plaid/mapper/LinkItemMapper.java
+++ plaid-service/src/main/java/com/backbase/proto/plaid/mapper/LinkItemMapper.java
@@ -6,6 +6,7 @@ import com.backbase.proto.plaid.model.Item;
 import com.backbase.proto.plaid.model.LinkItem;
 import com.backbase.proto.plaid.repository.AccountRepository;
 import com.backbase.proto.plaid.repository.InstitutionRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +15,20 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
+/**
+ * Maps Items that have been linked to an Item that is to be displayed in the app
+ */
 public class LinkItemMapper {
 
-    InstitutionRepository institutionRepository;
-    AccountRepository accountRepository;
+    private final InstitutionRepository institutionRepository;
+    private final AccountRepository accountRepository;
 
+    /**
+     * maps Logged users Items for endpoint
+     * @param item from our repo
+     * @return Item to be returned from end point
+     */
     private LinkItem map(Item item){
         LinkItem itemForDisplay = new LinkItem();
         itemForDisplay.itemId(item.getItemId());
@@ -31,6 +41,11 @@ public class LinkItemMapper {
         return itemForDisplay;
     }
 
+    /**
+     * Maps a list of Items from our repo to a list to be sent from an end point
+     * @param items from repo
+     * @return items to be displayed in app
+     */
     public List<LinkItem> map(List<Item> items){
         return items.stream().map(this::map).collect(Collectors.toList());
     }
